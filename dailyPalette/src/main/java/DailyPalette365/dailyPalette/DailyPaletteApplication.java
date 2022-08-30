319e461d321fb46c5558fee13db3f0a1f995e1b1
maplejh <cjh0313c@gmail.com>
2022-08-30 13:48:57 +0900

    Initial commit

---
+++ dailyPalette/src/main/java/DailyPalette365/dailyPalette/DailyPaletteApplication.java
@@ -0,0 +1,13 @@
+package DailyPalette365.dailyPalette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyPaletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyPaletteApplication.class, args);
+	}
+
+}
---
+++ dailyPalette/src/test/java/DailyPalette365/dailyPalette/DailyPaletteApplicationTests.java
@@ -0,0 +1,13 @@
+package DailyPalette365.dailyPalette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyPaletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
